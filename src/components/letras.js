d769263d3c521498c369524bdac1500db2ae8ba3
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-02-27 16:15:53 -0300

    guess word complete

--- src/components/letras.js
+++ src/components/letras.js
@@ -3,19 +3,19 @@ import Alfabeto from "./alfabeto";
 
 export default function Letras(props) {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const [chute,setChute]=useState("");
+    const [chute, setChute] = useState("");
 
-    function chutar(){
+    function chutar() {
         props.setFim(true)
-        if(chute===props.palavraEscolhida){
+        if (chute === props.palavraEscolhida) {
             console.log("bom chute");
 
             for (let i = 0; i < props.palavraEscolhida.length; i++) {
                 props.letrasEscolhida.push(Array.from(props.palavraEscolhida)[i]);
-                console.log("array pós chute",props.letrasEscolhida)
+                console.log("array pós chute", props.letrasEscolhida)
             }
-            
-        }else{
+
+        } else {
             console.log("errooou feio, errooou rude");
             props.setErros(6);
         }
@@ -32,8 +32,8 @@ export default function Letras(props) {
             </div>
             <div className="chute">
                 Já sei a palavra!
-                <input type="text" id="fname" name="fname" data-test="guess-input" value={chute} onChange={(e)=>setChute(e.target.value)}></input>
-                <button type="button" data-test="guess-button" onClick={()=>chutar()}><p>Chutar</p></button>
+                <input type="text" id="fname" name="fname" data-test="guess-input" value={chute} onChange={(e) => setChute(e.target.value)} disabled={(props.palavraEscolhida && props.fim === false) ? false : true}></input>
+                <button type="button" data-test="guess-button" onClick={() => chutar()} disabled={(props.palavraEscolhida && props.fim === false) ? false : true}><p>Chutar</p></button>
             </div>
         </div>
     )
